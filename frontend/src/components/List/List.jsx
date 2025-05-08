30ebb5cec0a2739b3efaf809d72a70b59ba9a0c8
Invictusmikey <invictusmikey@kali.com>
2025-05-08 15:20:16 -0500

    Se termina el diseño del proyecto

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -2,12 +2,14 @@ import { Buttons } from "../Buttons/Buttons";
 
 export const List = ({ texto }) => {
   return (
-    <div className="d-flex justify-content-center border border-primary rounded p-3 my-2 w-50">
-      <p className="text-center mb-0 ">
-        {texto}
-      </p>
-      <Buttons  status={"pendiente"}/>
+    <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-center justify-content-between border border-primary rounded p-3 my-2 w-75">
+        <p className="text-center mb-0 ">
+          {texto}
+        </p>
+        <Buttons status={"pendiente"} />
 
+      </div>
     </div>
   );
 };
--- frontend/src/components/Title/Title.jsx
+++ frontend/src/components/Title/Title.jsx
@@ -1,5 +1,5 @@
 
-export const Title = ({children}) => {
+export const Title = ({ children }) => {
     return (
         <div>
             <h1 className='nav justify-content-center p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 fuente-elegante' >{children}</h1>
